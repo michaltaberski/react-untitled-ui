609e3a68b98db032647e6509b1947ac1cf500c1c
Michał Taberski <m.taberski@gmail.com>
2024-04-20 17:28:49 +0200

    demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-// Styles
-import 'sanitize.css';
-import '@fontsource/inter/500.css';
-import '@radix-ui/themes/layout.css';
-
-import { Theme } from '@radix-ui/themes';
 import { DemoBox } from './DemoBox';
 
 function App() {
-  return (
-    <Theme>
-      <DemoBox m="2">Demo box</DemoBox>
-    </Theme>
-  );
+  return <DemoBox m="2">Demo box</DemoBox>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
+// Styles
+import 'sanitize.css';
+import '@fontsource/inter/500.css';
+import '@radix-ui/themes/layout.css';
+import './index.css';
+
+import { Theme } from '@radix-ui/themes';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Theme>
+      <App />
+    </Theme>
   </React.StrictMode>,
 );
