9d43c0efd51a255a4c06a94a8a1f2cb1f08fa2fb
Michał Taberski <m.taberski@gmail.com>
2024-04-20 17:11:47 +0200

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 // Styles
 import '@radix-ui/themes/layout.css';
+// https://fontsource.org/fonts/inter/install
+import '@fontsource/inter/500.css';
 
 import { Theme } from '@radix-ui/themes';
 import { DemoBox } from './DemoBox';
